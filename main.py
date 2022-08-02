8ce49904f0045d23de5aadc0c0ccdc7364ad8ae5
liborhavranek <liborhavranek91@gmail.com>
2022-08-02 08:12:13 +0200

    create reset of a game still not done

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import tkinter.messagebox
 from tkinter import *
 import random
 
 life = 7
-
+number_of_games = 0
 
 window = Tk()
 window.title("Hangman", )
 window.config(background="light blue")
 hangman_label = Label(text="Hangman", font=('Arial', 60), background="light blue", highlightthickness=0)
 hangman_label.grid(column=0, row=0, columnspan=3)
-points_label = Label(text="Počet uhodnutych slov: 0",
+points_label = Label(text=f"Počet uhodnutych slov: {number_of_games}",
                           font=('Arial', 20),
                           background="light blue",
                           highlightthickness=0)
@@ -24,10 +25,11 @@ hangman_4_image = PhotoImage(file="art/hangman_4.png")
 hangman_5_image = PhotoImage(file="art/hangman_5.png")
 hangman_6_image = PhotoImage(file="art/hangman_6.png")
 hangman_7_image = PhotoImage(file="art/hangman_7.png")
+hangman_win_image = PhotoImage(file="art/win.png")
 hangman_canvas.create_image(260, 160, image=hangman_7_image)
 hangman_canvas.grid(column=0, row=2, padx=20, columnspan=3)
 
-words = ["ahoj", "anakonda"]
+words = ["aj"]
 
 random_word = random.choice(words)
 print(random_word)
@@ -53,14 +55,14 @@ separation_label = Label(text="Vloz písmeno: ", font=('Arial', 40), background=
 separation_label.grid(column=0, row=4)
 
 
+def win():
+	if word_letters == display:
+		hangman_canvas.create_image(260, 160, image=hangman_win_image)
+		reset()
 
-def send():
+
+def loose():
 	global life
-	for position in range(len(random_word)):
-		pismenko = random_word[position]
-		if pismenko == entry_label.get():
-			display[position] = pismenko
-			canvas_text.itemconfig(text, text=f"{' '.join(display)}")
 	if entry_label.get() not in word_letters:
 		life -= 1
 		if life == 6:
@@ -78,6 +80,23 @@ def send():
 		elif life == 0:
 			hangman_canvas.create_image(260, 160, image=hangman_0_image)
 
+def reset():
+	global number_of_games
+	result = tkinter.messagebox.askquestion(("new game", "Are you sure you want to start a new game ?"))
+	if result == "Yes":
+		number_of_games += 1
+		print(number_of_games)
+
+def send():
+
+	for position in range(len(random_word)):
+		pismenko = random_word[position]
+		if pismenko == entry_label.get():
+			display[position] = pismenko
+			canvas_text.itemconfig(text, text=f"{' '.join(display)}")
+		win()
+	loose()
+
 entry_label = Entry(window, font=('Arial', 25), width=10)
 entry_label.grid(column=1, row=4)
 
@@ -85,6 +104,7 @@ send_button = Button(window, text="Odeslat", command=send)
 send_button.grid(column=2, row=4, padx=10)
 
 window.mainloop()
+
 
 
 
